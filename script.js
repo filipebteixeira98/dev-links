99c8e2c7409fb3e93a690ef40f049b4138705e26
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-06-11 14:08:35 -0300

    chore(core): change profile images when theme is switched

--- script.js
+++ script.js
@@ -3,19 +3,29 @@
 
   if (mode === 'light') {
     document.documentElement.classList.add('light');
+
+    document.querySelector('#profile img').setAttribute('src', './assets/avatar-light.png');
   }
 })();
 
 function toggleMode() {
   const html = document.documentElement;
 
+  const imgElement = document.querySelector('#profile img');
+
   if (html.classList.contains('light')) {
     html.classList.remove('light');
+
     localStorage.setItem('DevLinks:mode', 'dark');
+
+    imgElement.setAttribute('src', './assets/avatar.png');
   }
   else {
     html.classList.add('light');
+
     localStorage.setItem('DevLinks:mode', 'light');
+
+    imgElement.setAttribute('src', './assets/avatar-light.png');
   }
 
   // html.classList.toggle('light');
